3e9edb93f16f4459435dd34715b708489b62f0f8
AST9PIR2 <92747125+AST9PIR2@users.noreply.github.com>
2022-11-24 22:44:01 +0100

    refactoring observer_Tutorial

--- Patterns/src/main/java/se1/patterns/observer_Tutorial/GrabStocks.java
+++ Patterns/src/main/java/se1/patterns/observer_Tutorial/GrabStocks.java
@@ -7,14 +7,7 @@ public class GrabStocks {
         public static void main(String[] args) {
             var stockGrabber = new StockGrabber();
             var observer1 = new StockObserver(stockGrabber);
-            var observer2 = new StockObserver(stockGrabber);
-            var observer3 = new StockObserver(stockGrabber);
-            var observer4 = new StockObserver(stockGrabber);
 
-
-            stockGrabber.setIbmPrice(197.00);
-            stockGrabber.setAaplPrice(677.60);
-            stockGrabber.setGoogPrice(676.40);
 
 
             stockGrabber.setIbmPrice(197.00);
@@ -23,13 +16,7 @@ public class GrabStocks {
 
             stockGrabber.unregister(observer1);
 
-            stockGrabber.setIbmPrice(197.00);
-            stockGrabber.setAaplPrice(677.60);
-            stockGrabber.setGoogPrice(676.40);
 
-            stockGrabber.unregister(observer4);
-            stockGrabber.unregister(observer3);
-            stockGrabber.unregister(observer2);
 
         }
 }
