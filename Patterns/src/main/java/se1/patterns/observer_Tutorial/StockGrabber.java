c8cd20e3f467883d572c721dd3a5f748196a3757
AST9PIR2 <92747125+AST9PIR2@users.noreply.github.com>
2022-11-24 22:49:05 +0100

    refactoring observer_Tutorial

--- Patterns/src/main/java/se1/patterns/observer_Tutorial/StockGrabber.java
+++ Patterns/src/main/java/se1/patterns/observer_Tutorial/StockGrabber.java
@@ -4,62 +4,45 @@ import java.util.ArrayList;
 
 public class StockGrabber implements Subject{
 
-    private ArrayList<Observer> observers;
+    private final ArrayList<Observer> observers;
     private double ibmPrice;
     private double aaplPrice;
     private double googPrice;
 
     public StockGrabber() {
-
-        observers = new ArrayList<Observer>();
-
+        observers = new ArrayList<>();
     }
     @Override
     public void register(Observer newObserver) {
-
         observers.add(newObserver);
-
     }
 
     @Override
     public void unregister(Observer deleteObserver) {
-
         int observerIndex = observers.indexOf(deleteObserver);
-
         System.out.println("Observer " + (observerIndex+1) + " deleted");
-
         observers.remove(observerIndex);
-
     }
 
     @Override
     public void notifyObservers() {
-
-
         for(Observer observer : observers){
             observer.update(ibmPrice, aaplPrice, googPrice);
         }
-
     }
 
     public void setIbmPrice(double newIbmPrice){
-
         this.ibmPrice = newIbmPrice;
         notifyObservers();
-
     }
 
     public void setAaplPrice(double newAaplPrice){
-
         this.aaplPrice = newAaplPrice;
         notifyObservers();
-
     }
 
     public void setGoogPrice(double newGoogPrice){
-
         this.googPrice = newGoogPrice;
         notifyObservers();
-
     }
 }
--- Patterns/src/main/java/se1/patterns/observer_Tutorial/Subject.java
+++ Patterns/src/main/java/se1/patterns/observer_Tutorial/Subject.java
@@ -2,7 +2,7 @@ package se1.patterns.observer_Tutorial;
 
 public interface Subject {
 
-    public void register(Observer o);
-    public void unregister(Observer o);
-    public void notifyObservers();
+    void register(Observer o);
+    void unregister(Observer o);
+    void notifyObservers();
 }
